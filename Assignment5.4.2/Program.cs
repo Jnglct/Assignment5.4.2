83a3c9aacdb578e03739766d066c340c5b8aad11
Jnglct <rogerscole99@gmail.com>
2025-08-07 16:46:24 -0500

    Add project files.

---
+++ Assignment5.4.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Linq;
+
+namespace Assignment5._4._2
+{
+    internal class Program
+    {
+        static void Squarearrray(int[,] arr)
+        {
+            int sum = 0;
+            Console.WriteLine("The matrix is :");
+         
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{arr[i, j]} ");
+                }
+                Console.WriteLine();
+                sum += arr[i, i];
+            }
+            Console.Write("Addition of the right Diagonal elements is: ");
+            Console.Write(sum);
+        }
+        static void Main(string[] args)
+        {
+            Squarearrray(new int[,] { { 1, 2 },{ 3, 4 } });
+        }
+    }
+}
